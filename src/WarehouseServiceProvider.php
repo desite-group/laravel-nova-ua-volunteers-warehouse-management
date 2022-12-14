527bdfcc98de4186e9557ef410dd1a7d4b35c24d
RomanZhyla <roman.zhyla.lviv@gmail.com>
2022-12-14 23:01:09 +0200

    removed cloud

--- src/WarehouseServiceProvider.php
+++ src/WarehouseServiceProvider.php
@@ -10,14 +10,6 @@ use Laravel\Nova\Nova;
 
 class WarehouseServiceProvider extends ServiceProvider
 {
-    protected static $callbacks = [
-        Category::class => ['booted' => null],
-    ];
-
-    protected $policies = [
-        \DesiteGroup\LaravelWarehouseManagement\Models\Category::class => \DesiteGroup\LaravelWarehouseManagement\Policies\CategoryPolicy::class,
-    ];
-
     public function boot()
     {
         if (! class_exists('CreateWarehouseCategoriesTable')) {
@@ -28,56 +20,13 @@ class WarehouseServiceProvider extends ServiceProvider
 
         Nova::resourcesIn(__DIR__.'/Nova');
 
-//        $this->app->booted(function () {
-//
-//            $mod = app(NovaCategory::class);
-//            $final_class = get_class($mod);
-//
-//            Nova::resources([
-//                $final_class,
-//            ]);
-//
-//            $this->app->bind(Category::class, Category::class);
-//        });
+        $this->app->booted(function () {
 
-        $this->registerPolicies();
-    }
+            Nova::resources([
+                NovaCategory::class,
+            ]);
 
-    public function registerPolicies()
-    {
-        foreach ($this->policies() as $key => $value) {
-            Gate::policy($key, $value);
-        }
-    }
-
-    public function policies()
-    {
-        return $this->policies;
-    }
-
-    public static function setCallback($class, $hook, callable $cb)
-    {
-        if (!isset(self::$callbacks[$class])) {
-            throw new \Exception("setCallback::{$class} not supported");
-        }
-        $cl = self::$callbacks[$class];
-        if (!array_key_exists($hook, $cl)) {
-
-            throw new \Exception("setCallback::{$class}::{$hook} not supported");
-        }
-        self::$callbacks[$class][$hook] = $cb;
-    }
-    public static function getCallback($class, $hook)
-    {
-        $default = function () {
-        };
-        if (!isset(self::$callbacks[$class])) {
-            return $default;
-        }
-        $cl = self::$callbacks[$class];
-        if (!isset($cl[$hook])) {
-            return $default;
-        }
-        return $cl[$hook];
+            $this->app->bind(Category::class, Category::class);
+        });
     }
 }
