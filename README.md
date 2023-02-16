08de954e8750fbde80767a6c98e8dbd6dd1e861a
RomanZhyla <roman.zhyla.lviv@gmail.com>
2023-02-16 16:07:42 +0200

    changed conception

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Laravel Nova Warehouse Management
+# Laravel Nova UA Volunteers Warehouse Management
 A simple package for an online store, for managing orders and warehouse. Based on Laravel Nova
 
 # Installation
@@ -9,7 +9,7 @@ You can install the package via composer:
 
 You need to publish the migration to create tables:
 
-    php artisan vendor:publish --provider="DesiteGroup\LaravelNovaWarehouseManagement\WarehouseServiceProvider" --tag="migrations"
+    php artisan vendor:publish --provider="DesiteGroup\LaravelNovaWarehouseManagement\Providers\WarehouseServiceProvider" --tag="migrations"
 
 After that, you need to run migrations.
 
--- src/Providers/WarehouseServiceProvider.php
+++ src/Providers/WarehouseServiceProvider.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace DesiteGroup\LaravelNovaWarehouseManagement;
+namespace DesiteGroup\LaravelNovaWarehouseManagement\Providers;
 
 use DesiteGroup\LaravelNovaWarehouseManagement\Nova\Category as NovaCategory;
 use DesiteGroup\LaravelNovaWarehouseManagement\Nova\Product as NovaProduct;
@@ -16,31 +16,31 @@ class WarehouseServiceProvider extends ServiceProvider
     {
         if (! class_exists('CreateWarehouseCategoriesTable')) {
             $this->publishes([
-                __DIR__.'/../database/migrations/create_warehouse_categories_table.php.stub' => database_path('migrations/'.date('Y_m_d_His', time()).'_create_warehouse_categories_table.php'),
+                __DIR__ . '/../database/migrations/create_warehouse_categories_table.php.stub' => database_path('migrations/'.date('Y_m_d_His', time()).'_create_warehouse_categories_table.php'),
            ], 'migrations');
         }
 
         if (! class_exists('CreateWarehouseProductsTable')) {
             $this->publishes([
-                __DIR__.'/../database/migrations/create_warehouse_products_table.php.stub' => database_path('migrations/'.date('Y_m_d_His', time()).'_create_warehouse_products_table.php'),
+                __DIR__ . '/../database/migrations/create_warehouse_products_table.php.stub' => database_path('migrations/'.date('Y_m_d_His', time()).'_create_warehouse_products_table.php'),
             ], 'migrations');
         }
 
         if (! class_exists('CreateWarehouseCounteragentsTable')) {
             $this->publishes([
-                __DIR__.'/../database/migrations/create_warehouse_counteragents_table.php.stub' => database_path('migrations/'.date('Y_m_d_His', time()).'_create_warehouse_counteragents_table.php'),
+                __DIR__ . '/../database/migrations/create_warehouse_counteragents_table.php.stub' => database_path('migrations/'.date('Y_m_d_His', time()).'_create_warehouse_counteragents_table.php'),
             ], 'migrations');
         }
 
         if (! class_exists('CreateWarehouseApplicationsTable')) {
             $this->publishes([
-                __DIR__.'/../database/migrations/create_warehouse_applications_table.php.stub' => database_path('migrations/'.date('Y_m_d_His', time()).'_create_warehouse_applications_table.php'),
+                __DIR__ . '/../database/migrations/create_warehouse_applications_table.php.stub' => database_path('migrations/'.date('Y_m_d_His', time()).'_create_warehouse_applications_table.php'),
             ], 'migrations');
         }
 
         if (! class_exists('CreateWarehouseActsTable')) {
             $this->publishes([
-                __DIR__.'/../database/migrations/create_warehouse_acts_table.php.stub' => database_path('migrations/'.date('Y_m_d_His', time()).'_create_warehouse_acts_table.php'),
+                __DIR__ . '/../database/migrations/create_warehouse_acts_table.php.stub' => database_path('migrations/'.date('Y_m_d_His', time()).'_create_warehouse_acts_table.php'),
             ], 'migrations');
         }
 
