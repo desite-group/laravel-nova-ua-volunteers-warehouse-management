9ae7f5baaedafe99c9a0c165ce43e34b9e1a7f43
RomanZhyla <roman.zhyla.lviv@gmail.com>
2023-02-22 17:20:04 +0200

    fixed provider migrations

--- src/Providers/WarehouseServiceProvider.php
+++ src/Providers/WarehouseServiceProvider.php
@@ -16,31 +16,31 @@ class WarehouseServiceProvider extends ServiceProvider
     {
         if (! class_exists('CreateWarehouseCategoriesTable')) {
             $this->publishes([
-                __DIR__ . '/../database/migrations/create_warehouse_categories_table.php.stub' => database_path('migrations/'.date('Y_m_d_His', time()).'_create_warehouse_categories_table.php'),
+                __DIR__ . '/../database/migrations/create_volunteers_warehouse_categories_table.php.stub' => database_path('migrations/'.date('Y_m_d_His', time()).'_create_volunteers_warehouse_categories_table.php'),
            ], 'migrations');
         }
 
         if (! class_exists('CreateWarehouseProductsTable')) {
             $this->publishes([
-                __DIR__ . '/../database/migrations/create_warehouse_products_table.php.stub' => database_path('migrations/'.date('Y_m_d_His', time()).'_create_warehouse_products_table.php'),
+                __DIR__ . '/../database/migrations/create_volunteers_warehouse_products_table.php.stub' => database_path('migrations/'.date('Y_m_d_His', time()).'_create_volunteers_warehouse_products_table.php'),
             ], 'migrations');
         }
 
         if (! class_exists('CreateWarehouseCounteragentsTable')) {
             $this->publishes([
-                __DIR__ . '/../database/migrations/create_warehouse_counteragents_table.php.stub' => database_path('migrations/'.date('Y_m_d_His', time()).'_create_warehouse_counteragents_table.php'),
+                __DIR__ . '/../database/migrations/create_volunteers_warehouse_counteragents_table.php.stub' => database_path('migrations/'.date('Y_m_d_His', time()).'_create_volunteers_warehouse_counteragents_table.php'),
             ], 'migrations');
         }
 
         if (! class_exists('CreateWarehouseApplicationsTable')) {
             $this->publishes([
-                __DIR__ . '/../database/migrations/create_warehouse_applications_table.php.stub' => database_path('migrations/'.date('Y_m_d_His', time()).'_create_warehouse_applications_table.php'),
+                __DIR__ . '/../database/migrations/create_volunteers_warehouse_applications_table.php.stub' => database_path('migrations/'.date('Y_m_d_His', time()).'_create_volunteers_warehouse_applications_table.php'),
             ], 'migrations');
         }
 
         if (! class_exists('CreateWarehouseActsTable')) {
             $this->publishes([
-                __DIR__ . '/../database/migrations/create_warehouse_acts_table.php.stub' => database_path('migrations/'.date('Y_m_d_His', time()).'_create_warehouse_acts_table.php'),
+                __DIR__ . '/../database/migrations/create_volunteers_warehouse_acts_table.php.stub' => database_path('migrations/'.date('Y_m_d_His', time()).'_create_volunteers_warehouse_acts_table.php'),
             ], 'migrations');
         }
 
