9b1bbf2a77e288984a2943fd7c3b99ed04c1c714
RomanZhyla <roman.zhyla.lviv@gmail.com>
2023-06-02 21:04:56 +0300

    removed sortable

--- src/Models/MeasurementUnit.php
+++ src/Models/MeasurementUnit.php
@@ -8,42 +8,12 @@ use Spatie\Image\Manipulations;
 use Spatie\MediaLibrary\HasMedia;
 use Spatie\MediaLibrary\InteractsWithMedia;
 use Spatie\MediaLibrary\MediaCollections\Models\Media;
-use Spatie\EloquentSortable\Sortable;
-use Spatie\EloquentSortable\SortableTrait;
 
-class MeasurementUnit extends Model implements Sortable, HasMedia
+class MeasurementUnit extends Model implements HasMedia
 {
-    use HasFactory, InteractsWithMedia, SortableTrait;
+    use HasFactory, InteractsWithMedia;
 
     protected $fillable = [
         'title', 'description', 'code', 'is_active', 'is_general'
     ];
-
-    public $sortable = [
-        'order_column_name' => 'sort_order',
-        'sort_when_creating' => true,
-    ];
-
-    public function scopeOrdered($query)
-    {
-        return $query->orderBy('sort_order');
-    }
-
-    public function registerMediaConversions(Media $media = null): void
-    {
-        $this->addMediaConversion('main')
-            ->format(Manipulations::FORMAT_JPG)
-            ->fit(Manipulations::FIT_MAX, 800, 800)
-            ->performOnCollections('photo');
-
-        $this->addMediaConversion('thumb_main')
-            ->format(Manipulations::FORMAT_JPG)
-            ->fit(Manipulations::FIT_MAX, 20, 20)
-            ->performOnCollections('photo');
-    }
-
-    public function registerMediaCollections(): void
-    {
-        $this->addMediaCollection('photo')->singleFile();
-    }
 }
